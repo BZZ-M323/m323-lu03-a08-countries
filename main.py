4ad91ad26b2c212dd1562c54c386abcae493b496
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-12 16:23:01 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,85 @@
-def calculate():
+def analyze_countries(data, filter_func, transform_func):
     """
-    calculates the total
-    :return: None
+    Analysiert eine Liste von Länderdaten. Verwendet 'filter_func' zum Filtern und 'transform_func' zum Transformieren der Länderdaten.
+
+    Args:
+    - data: Liste von Ländern als Dictionaries
+    - filter_func: Funktion zum Filtern der Länder
+    - transform_func: Funktion zum Transformieren der Länderdaten
+
+    Returns:
+    - Liste von transformierten Länderdaten
     """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+    # TODO: Implementiere die Funktion hier
+    pass
+
+
+def filter_european_countries(country):
+    """
+    Filtert europäische Länder aus der Liste.
+
+    Args:
+    - country: Ein Dictionary, das ein Land repräsentiert
+
+    Returns:
+    - True, wenn das Land in Europa liegt, sonst False
+    """
+    # TODO: Implementiere die Funktion hier
+    pass
+
+
+def filter_large_population_countries(country):
+    """
+    Filtert Länder mit einer Bevölkerung von mehr als 10 Millionen.
+
+    Args:
+    - country: Ein Dictionary, das ein Land repräsentiert
+
+    Returns:
+    - True, wenn das Land eine Bevölkerung von mehr als 10 Millionen hat, sonst False
+    """
+    # TODO: Implementiere die Funktion hier
+    pass
+
+
+def transform_to_name_and_capital(country):
+    """
+    Transformiert das Länder-Dictionary, sodass nur der Name und die Hauptstadt zurückgegeben werden.
+
+    Args:
+    - country: Ein Dictionary, das ein Land repräsentiert
+
+    Returns:
+    - Ein Dictionary mit dem Namen und der Hauptstadt des Landes
+    """
+    # TODO: Implementiere die Funktion hier
+    pass
+
+
+def transform_to_name_and_area(country):
+    """
+    Transformiert das Länder-Dictionary, sodass nur der Name und die Fläche zurückgegeben werden.
+
+    Args:
+    - country: Ein Dictionary, das ein Land repräsentiert
+
+    Returns:
+    - Ein Dictionary mit dem Namen und der Fläche des Landes
+    """
+    # TODO: Implementiere die Funktion hier
+    pass
 
 
 if __name__ == '__main__':
-    calculate()
+    """ Hauptfunktion zum Ausführen des Programms """
+    # TODO: JSON-Datei (countries_data.json) öffnen und Daten laden
+    countries_data = None
+
+    # Europäische Länder filtern und nach Name und Hauptstadt transformieren
+    european_countries = analyze_countries(countries_data, filter_european_countries, transform_to_name_and_capital)
+    print('Europäische Länder (Name und Hauptstadt):', european_countries)
+
+    # Länder mit mehr als 10 Millionen Einwohnern filtern und nach Name und Fläche transformieren
+    large_population_countries = analyze_countries(countries_data, filter_large_population_countries,
+                                                   transform_to_name_and_area)
+    print('Länder mit mehr als 10 Millionen Einwohnern (Name und Fläche):', large_population_countries)
--- main_test.py
+++ main_test.py
@@ -1,6 +1,33 @@
-import main
+import pytest
+import json
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+from main import *
+# Define test cases using real data
+
+
+class TestClassReport:
+
+    @pytest.fixture
+    def real_countries_data(self):
+        try:
+            with open('countries_data.json', 'r', encoding='utf-8') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print("Datei nicht gefunden!")
+
+    def test_analyze_countries_with_real_data(self, real_countries_data):
+        # Test filter and transformation functions with real data
+        european_countries = analyze_countries(real_countries_data, filter_european_countries,
+                                               transform_to_name_and_capital)
+        large_population_countries = analyze_countries(real_countries_data, filter_large_population_countries,
+                                                       transform_to_name_and_area)
+
+        # Test: Verify that the filtered European countries are actually from Europe and have the required keys
+        #assert all(country['region'] == 'Europe' for country in european_countries)
+        assert all('name' in country and 'capital' in country for country in european_countries)
+
+        # Test: Verify that the filtered countries have a population greater than 10 million and have the required keys
+        #assert all(country['population'] > 10000000 for country in large_population_countries)
+        assert all('name' in country and 'area' in country for country in large_population_countries)
+
+
